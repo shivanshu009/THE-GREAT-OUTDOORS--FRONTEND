903d066ff25a27514ab0657442fd203b096f863a
Prince6006 <shivanshu.chat421@gmail.com>
2023-12-23 19:42:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// let express =require('express')
+// let app = express()
+// let port =4000
+
+// app.use(express.static(__dirname + 'public'))
+
+// app.get('/',(req,res,next)=>{
+//     res.sendFile(__dirname + '/index.html')
+// })
+// app.get('/about',(req,res,next)=>{
+//     res.sendFile(__dirname + '/about.html')
+// })
+// app.get('/explore',(req,res,next)=>{
+//     res.sendFile(__dirname + '/explore.html')
+// })
+// app.get('/journal',(req,res,next)=>{
+//     res.sendFile(__dirname + '/journal.html')
+// })
+// app.get('/search',(req,res,next)=>{
+//     res.sendFile(__dirname + '/search.html')
+// })      
+// app.listen(port,()=>{
+//     console.log("server is running... ")
+// })
+
+var home = document.querySelector(".hm");
+home.addEventListener("mouseclick",function(){
+    pageYOffset
+
+})
